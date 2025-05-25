5abd5228abfe0f84a23c1bcca3c26dabc17c68dc
faithgaiciumia <faithgaiciumia078@gmail.com>
2025-05-25 11:23:10 +0300

    moved mongoose string to env

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 mongoose.connect(
-  "mongodb+srv://faith:feifei@cluster0.qx0uy.mongodb.net/wanlinq"
+  process.env.MONGOOSE_CONNECT_STRING
 );
 mongoose.connection.on("connected", function () {
   console.log("Application is connected to the database");
