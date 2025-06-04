cb9b6eb35f8e186a4d1afc210f5de0020884d4a6
faithgaiciumia <faithgaiciumia078@gmail.com>
2025-06-05 00:58:09 +0300

    add port

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ server.start().then(() => {
 
   const PORT = process.env.PORT || 4000;
 
-  app.listen(PORT, () => {
+  app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on http://0.0.0.0:${PORT}`);
   });
 });
