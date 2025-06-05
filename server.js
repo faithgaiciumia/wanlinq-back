7ea4b01c86163686fab9692485f5b16259e6475f
faithgaiciumia <faithgaiciumia078@gmail.com>
2025-06-05 19:11:05 +0300

    updated origins

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.set("trust proxy", true);
 // CORS setup
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://quicklinq.netlify.app/"],
+    origin: ["http://localhost:5173", "https://quicklinq.netlify.app"],
     credentials: true,
   })
 );
@@ -53,7 +53,7 @@ server.start().then(() => {
 
   const PORT = process.env.PORT || 4000;
 
-  app.listen(PORT, '0.0.0.0', () => {
+  app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on http://0.0.0.0:${PORT}`);
   });
 });
